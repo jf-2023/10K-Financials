e7b59af0a7f698989bf7aa7ae20dfffd7b346530
u1324382379870 <126424066+u1324382379870@users.noreply.github.com>
2023-03-19 15:51:30 -0800

    Update p1.py

--- p1.py
+++ p1.py
@@ -1,47 +1,62 @@
-#Get acces to API
+import pandas as pd
 import requests
+import time
+import matplotlib.pyplot as plt
+import seaborn as sns
+start = time.perf_counter()
+
 header = {'User-Agent': "mr.muffin235@gmail.com"}
-companyTickers = requests.get("https://www.sec.gov/files/company_tickers.json", headers=header)
 
-#Find data and assign ticker symbol to cik#
-import pandas as pd
-T_list = companyTickers.json().values() #Turn data in a dictionary
-symbol = input('Enter company ticker symbol: ').upper()
-for info in T_list:
-    if symbol == info['ticker']:
-        new = info['cik_str'] #if there is a match assign cik# to a new variable
-        print(f'{new:010d}') #format cik# with leading zeros (length of 10)
+def get_cik(symbol):
+    companyTickers = requests.get("https://www.sec.gov/files/company_tickers.json", headers=header)
+    T_list = companyTickers.json().values()
+    for list1 in T_list:
+        if list1['ticker'] == symbol:
+            cik = list1['cik_str']
+            return cik
 
-        
+def get_AccFY(acc, year, symbol):
+    companyFrames = requests.get((f'https://data.sec.gov/api/xbrl/frames/us-gaap/{acc}/USD/CY{year}Q4I.json'), headers=header)
+    frame = companyFrames.json()['data']
+    frameDF = pd.DataFrame(frame)
+    frameDF2 = frameDF.drop(columns=['accn', 'end', 'loc', 'entityName'])
+    frameDF2.insert(loc=0, column=symbol, value=acc)
+    frameDF2.rename(columns={'val': year}, inplace=True)
+    x = frameDF2[frameDF2['cik'] == get_cik(symbol)]
+    return x
 
-        
-        
-########################################        
-import pandas as pd
-account = ['AssetsCurrent', 'Liabilities']
-year = '2022'
+acc_list = ['AssetsCurrent','Liabilities']
 
-fin_dict = {'Account': [a for a in account]}
-for ticker in symbol_list:
-    fin_dict[ticker] = []
+def getDF_acc(list, year, symbol):
+    df_list = []
+    for acc in list:
+        i = get_AccFY(acc, year, symbol)
+        df_list.append(i)
+    newDF = pd.concat(df_list, ignore_index=True)
+    return newDF
 
+years_list = [2022, 2021, 2020, 2019, 2018, 2017, 2016]
+def getDf_yrs(AccList, list, symbol):
+    bs = getDF_acc(AccList, years_list[0], symbol)
+    bs[years_list[0]] = bs[years_list[0]].astype(float)
+    years_list.pop(0)
+    for year in list:
+        i = getDF_acc(AccList, year, symbol)
+        i[year] = i[year].astype(float)
+        bs = pd.merge(bs, i)
+    bs.drop(columns='cik', inplace=True)
+    return bs
 
-for acc in account:
-    companyFrames = requests.get((f'https://data.sec.gov/api/xbrl/frames/us-gaap/{acc}/USD/CY{year}Q4I.json'),
-                                 headers=header)
-    data = companyFrames.json()['data']
-    count = 0
-    for cik in cik_list:
-        for data_list in data:
-            if cik == data_list['cik']:
-                num = format(data_list['val'], ',d')
-                fin_dict[symbol_list[count]] += [num]
-                count += 1
-                break
-print(fin_dict)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+pd.options.display.float_format = '{:,}'.format
 
-pd.set_option('display.max_columns', None)
-fdata_DF = pd.DataFrame(fin_dict)
-fdata_DF.set_index('Account', inplace=True)
-z = fdata_DF.rename(index={'RetainedEarningsAccumulatedDeficit': 'RetainedEarnings'})
-print(z)
+df = getDf_yrs(acc_list, years_list, 'META')
+df2 = df.T
+df2['Acid'] = df['AssetsCurrent'] / df['Liabilities']
+print(df2)
+#sns.lineplot(data=df)
+#plt.show()
+
+end = time.perf_counter()
+print(f"Runtime:{end - start:.3}s")
